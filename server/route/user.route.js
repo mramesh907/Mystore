03d81a345e32a8639538d59b35e77f43832c3982
Ramesh Maity <rcreation688@gmail.com>
2024-12-01 17:35:41 +0530

    changes

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -18,6 +18,7 @@ import upload from '../middleware/multer.middleware.js';
 const userRouter = Router();
 
 userRouter.post('/register', registerUserController);
+userRouter.post('/verify-email', verifyEmailController);
 userRouter.post('/login', loginController);
 
 userRouter.put('/forgot-password', forgotPasswordController);
