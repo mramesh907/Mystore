24b0592fee6a1238dd67877f3c2b8ad10dd6432f
Ramesh Maity <rcreation688@gmail.com>
2024-12-02 00:49:33 +0530

    verifyemail api added

--- client/src/common/SummartApi.js
+++ client/src/common/SummartApi.js
@@ -5,6 +5,13 @@ const SummaryApi = {
     url: '/api/user/register',
     method: 'post',
   },
+  verifyEmail: {
+    url: '/api/user/verify-email',
+    method: 'post',
+    params: {
+      code: '', // This will be populated dynamically when calling the endpoint
+    },
+  },
   login: {
     url: '/api/user/login',
     method: 'post',
