49eb29ac77eaa5cf97a18f3b29abef498e2d1f3e
Ramesh Maity <rcreation688@gmail.com>
2024-12-03 11:15:10 +0530

    change verify email structure

--- server/utils/verifyEmailTemplate.js
+++ server/utils/verifyEmailTemplate.js
@@ -1,12 +1,18 @@
 const verifyEmailTemplate = ({ name, userId }) => {
   let url = `${process.env.FRONTEND_URL}/verify-email?code=${userId}`;
   return `
-    <h1>Hi ${name}</h1>
-    <p>Click the link below to verify your email</p>
-    <a href="${url}" style="color:white;background-color:#071263; margin-top: 10px ;padding: 20px;display:block"> 
-    Verify Email
-     </a>
-    
-    `;
+    <div>
+      <p>Dear ${name},</p>
+      <p>Click the link below to verify your email:</p>
+      <a href="${url}" style="color: white; background-color: #071263; padding: 20px; display: block; text-align: center; text-decoration: none; margin-top: 10px;">
+        Verify Email
+      </a>
+      <br />
+      <p>If you did not request this, please ignore this email.</p>
+      <p>Thanks,</p>
+      <p>Mystore</p>
+    </div>
+  `;
 };
+
 export default verifyEmailTemplate;
