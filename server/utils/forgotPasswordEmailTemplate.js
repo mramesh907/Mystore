eb1ca03d6e9de513d09c41d1dd28c7aa47be74b8
Ramesh Maity <rcreation688@gmail.com>
2024-12-03 11:15:34 +0530

    chnages time for fotget email template

--- server/utils/forgotPasswordEmailTemplate.js
+++ server/utils/forgotPasswordEmailTemplate.js
@@ -4,7 +4,7 @@ const forgotPasswordEmailTemplate = ({ name, otp }) => {
         <p>Dear ${name}</p>
         <p>Use the following OTP to reset your password</p>
         <h1 style="text-align: center ;background-color: yellow; padding: 10px ">${otp}</h1>
-        <p>This OTP is valid for 1 hour.Enter this otp in Mystore to proceed with resetting your password </p>
+        <p>This OTP is valid for 2 minutes.Enter this otp in Mystore to proceed with resetting your password </p>
         <br/>
         </br>
         <p>Thanks</p>
