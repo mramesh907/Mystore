d58be27cb88c529de14e48b23a37d869c64fea6a
Ramesh Maity <rcreation688@gmail.com>
2024-12-05 00:44:17 +0530

    add loadingCategory in store

--- client/src/store/productSlice.js
+++ client/src/store/productSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialValue = {
     allCategory : [],
+    loadingCategory : false,
     allSubCategory : [],
     product : []
 }
@@ -13,6 +14,10 @@ const productSlice = createSlice({
         setAllCategory : (state,action) => {
             state.allCategory = [...action.payload];
         },
+        setLoadingCategory : (state,action) => {
+            state.loadingCategory = action.payload
+        }
+        ,
         setAllSubCategory : (state,action)=>{
             state.allSubCategory = [...action.payload];
         }
@@ -25,6 +30,7 @@ const productSlice = createSlice({
     }
 })
 
-export const { setAllCategory, setAllSubCategory } = productSlice.actions;
+export const { setAllCategory, setAllSubCategory, setLoadingCategory } =
+  productSlice.actions;
 
 export default productSlice.reducer
